68f15254d485c1d4a7a4a6edd4d0be625bbe4e4e
trswork <87744214+trswork@users.noreply.github.com>
2021-09-11 12:09:39 -0400

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,14 @@
 var city="";
-
+// variable declaration
 var searchCity = $("#search-city");
 var searchButton = $("#search-button");
 var clearButton = $("#clear-history");
 var currentCity = $("#current-city");
 var currentTemperature = $("#temperature");
 var currentHumidty= $("#humidity");
-var currentWSpeed= $("#wind-speed");
+var currentWSpeed=$("#wind-speed");
 var currentUvindex= $("#uv-index");
 var sCity=[];
-
 // searches the city to see if it exists in the entries from the storage
 function find(c){
     for (var i=0; i<sCity.length; i++){
@@ -19,9 +18,9 @@ function find(c){
     }
     return 1;
 }
-
-var APIKey="AIzaSyCTCdiegt4PCH9JKq5IpdYa8OIiRXPEffU";
-
+//Set up the API key
+var APIKey="a0aca8a89948154a4182dcecc780b513";
+// Display the curent and future weather to the user after grabing the city form the input text box.
 function displayWeather(event){
     event.preventDefault();
     if(searchCity.val().trim()!==""){
@@ -29,38 +28,37 @@ function displayWeather(event){
         currentWeather(city);
     }
 }
-
+// Here we create the AJAX call
 function currentWeather(city){
+    // Here we build the URL so we can get a data from server side.
     var queryURL= "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=" + APIKey;
     $.ajax({
         url:queryURL,
         method:"GET",
     }).then(function(response){
 
+        // parse the response to display the current weather including the City name. the Date and the weather icon. 
         console.log(response);
-
+        //Dta object from server side Api for icon property.
         var weathericon= response.weather[0].icon;
         var iconurl="https://openweathermap.org/img/wn/"+weathericon +"@2x.png";
-
+        // The date format method is taken from the  https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date
         var date=new Date(response.dt*1000).toLocaleDateString();
-
+        //parse the response for name of city and concanatig the date and icon.
         $(currentCity).html(response.name +"("+date+")" + "<img src="+iconurl+">");
+        // parse the response to display the current temperature.
+        // Convert the temp to fahrenheit
 
         var tempF = (response.main.temp - 273.15) * 1.80 + 32;
         $(currentTemperature).html((tempF).toFixed(2)+"&#8457");
-        
         // Display the Humidity
-        
         $(currentHumidty).html(response.main.humidity+"%");
-        
         //Display Wind speed and convert to MPH
-        
         var ws=response.wind.speed;
         var windsmph=(ws*2.237).toFixed(1);
         $(currentWSpeed).html(windsmph+"MPH");
-        
         // Display UVIndex.
-       
+        //By Geographic coordinates method and using appid and coordinates as a parameter we are going build our uv query url inside the function below.
         UVIndex(response.coord.lon,response.coord.lat);
         forecast(response.id);
         if(response.cod==200){
@@ -81,11 +79,12 @@ function currentWeather(city){
                 }
             }
         }
-    })
-}
 
+    });
+}
+    // This function returns the UVIindex response.
 function UVIndex(ln,lt){
-    
+    //lets build the url for uvindex.
     var uvqURL="https://api.openweathermap.org/data/2.5/uvi?appid="+ APIKey+"&lat="+lt+"&lon="+ln;
     $.ajax({
             url:uvqURL,
@@ -94,7 +93,7 @@ function UVIndex(ln,lt){
                 $(currentUvindex).html(response.value);
             });
 }
-
+    
 // Here we display the 5 days forecast for the current city.
 function forecast(cityid){
     var dayover= false;
@@ -121,13 +120,14 @@ function forecast(cityid){
     });
 }
 
+//Daynamically add the passed city on the search history
 function addToList(c){
     var listEl= $("<li>"+c.toUpperCase()+"</li>");
     $(listEl).attr("class","list-group-item");
     $(listEl).attr("data-value",c.toUpperCase());
     $(".list-group").append(listEl);
 }
-
+// display the past search again when the list group item is clicked in search history
 function invokePastSearch(event){
     var liEl=event.target;
     if (event.target.matches("li")){
